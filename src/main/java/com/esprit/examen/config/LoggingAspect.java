b8e6faa31d118a6b78023a480c048dfd1568f485
eya aouini <eyaaouini@hotmail.com>
2022-02-17 10:06:57 +0100

    my first commit

---
+++ src/main/java/com/esprit/examen/config/LoggingAspect.java
@@ -0,0 +1,35 @@
+package com.esprit.examen.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+	private static final Logger l = LogManager.getLogger(LoggingAspect.class);
+
+//lors l excution thesseb lwa9t
+	@Around("execution(* com.esprit.examen.services.*.*(..))")
+	public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object obj = pjp.proceed();
+		long elapsedTime = System.currentTimeMillis() - start;
+		l.info("Method execution time: " + elapsedTime + " milliseconds.");
+		return obj;
+	}
+
+//dima tkharej juste msg baaed matoufa l excution 
+	@After("execution(* com.esprit.examen.services.*.*(..))")
+	public void logMethodExit(JoinPoint joinPoint) {
+		String name = joinPoint.getSignature().getName();
+		l.info("méthode : " + name + " exécutée avec succès");
+	}
+
+}
